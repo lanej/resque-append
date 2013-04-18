fcc228fd614854938ac713d3c092d3cdbfeef7f4
Josh Lane <me@joshualane.com>
2013-04-17 17:10:28 -0700

    process work when enabled

--- lib/resque/append.rb
+++ lib/resque/append.rb
@@ -2,16 +2,48 @@ require "resque/append/version"
 
 module Resque
   module Append
-    def self.disable!; @enabled = false; end
-    def self.enable!; @enabled = true; end
-    def self.enabled; @enabled; end
-    def self.enabled?; !!@enabled; end
-    def self.enabled=(enabled); @enabled = !!enabled; end
-    def self.idle!; @processing = false; end
-    def self.processing!; @processing = true; end
-    def self.processing=(processing); @processing = !!processing; end
-    def self.processing?; !!@processing; end
-    def self.worker=(worker); @worker = worker; end
+    def self.disable!
+      self.enabled = false
+    end
+
+    def self.enable!
+      self.enabled = true
+    end
+
+    def self.enabled
+      @enabled
+    end
+
+    def self.enabled?
+      !!@enabled
+    end
+
+    def self.enabled=(enabled)
+      @enabled = !!enabled
+      Resque::Append.work if @enabled
+      @enabled
+    end
+
+    def self.idle!
+      @processing = false
+    end
+
+    def self.processing!
+      @processing = true
+    end
+
+    def self.processing=(processing)
+      @processing = !!processing
+    end
+
+    def self.processing?
+      !!@processing
+    end
+
+    def self.worker=(worker)
+      @worker = worker
+    end
+
     def self.worker
       @worker ||= Resque::Worker.new("*").tap do |w|
         w.cant_fork  = true
@@ -19,12 +51,21 @@ module Resque
       end
     end
 
-    def after_enqueue_append(*args)
+    def self.work
       if Resque::Append.enabled? && !Resque::Append.processing?
         Resque::Append.processing!
         Resque::Append.worker.work(0)
         Resque::Append.idle!
       end
+    end
+
+    def after_enqueue_append(*args)
+      Resque::Append.work
+    end
+
+    # resque-scheduler
+    def after_schedule_append(*args)
+      Resque.enqueue(self, *args)
     end
   end
 end
